6cf0261a9487fed628592d361a91e9058cb95350
Timur <100090508+Timik2t@users.noreply.github.com>
2023-05-31 00:31:51 +0300

    2

--- yacut/models.py
+++ yacut/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 from yacut import db
+from . constants import SHORT_DB_LENGTH
 
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(128), nullable=False)
-    short = db.Column(db.String(16), unique=True, nullable=False)
+    short = db.Column(db.String(SHORT_DB_LENGTH), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
--- yacut/views.py
+++ yacut/views.py
@@ -6,9 +6,10 @@ from flask import redirect, render_template, url_for
 from yacut import app, db
 from .forms import URLMapForm
 from .models import URLMap
+from .constants import SHORT_ID_LENGTH
 
 
-def get_unique_short_id(length=6):
+def get_unique_short_id(length=SHORT_ID_LENGTH):
     chars = string.ascii_letters + string.digits
     while True:
         short_id = ''.join(random.choices(chars, k=length))
@@ -20,22 +21,18 @@ def get_unique_short_id(length=6):
 def index():
     form = URLMapForm()
     if form.validate_on_submit():
-        original_link = form.original_link.data
         custom_id = form.custom_id.data
         if not custom_id:
             custom_id = get_unique_short_id()
-        url_map = URLMap(original=original_link, short=custom_id)
-        db.session.add(url_map)
+        db.session.add(URLMap(original=form.original_link.data, short=custom_id))
         db.session.commit()
         return redirect(url_for('redirect_to_url', short_id=custom_id))
     return render_template('index.html', form=form)
 
 
-@app.route('/<short_id>')
+@app.route('/<string:short_id>')
 def redirect_to_url(short_id):
-    url_map = URLMap.query.filter_by(short=short_id).first()
-    if url_map:
-        return redirect(url_map.original)
-    else:
-        # Обработка случая, когда короткий идентификатор не существует
+    url_map = URLMap.query.filter_by(short=short_id).first_or_404()
+    if not url_map:
         return render_template('not_found.html')
+    return redirect(url_map.original)
